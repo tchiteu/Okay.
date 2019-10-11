bd6ea8bbad884196346b0b32e9e4ddee22643701
tchiteu <matheus_santos9@joinville.sc.senai.br>
2019-10-11 19:46:24 -0300

    Inicio pagina login

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Okay.</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class='container-center'>
+        <form action="" class='login-form flex'>
+            <label for="iUser">Usuário/E-mail</label>
+            <input id="iUser" class="login-input" type="text" placeholder="Ex. matheussantos@email.com">
+            
+            <label for="iSenha">Senha</label>
+            <input id="iSenha" class="login-input" type="password" placeholder="Minímo 6 caracteres">
+            <input type="submit" value="Entrar">
+        </form>
+    </div>
+</body>
+</html>
