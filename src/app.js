aa64bc6f2c924bf2efb68de201b63b2a223207a7
tchiteu <matheus_santos9@joinville.sc.senai.br>
2019-11-04 23:11:14 -0200

    Adicionado página de cadastro

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 app.use(express.static(__dirname + "/../public"));
 
-app.get('/login', function(req, res) {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/index.html"));
 });
 
-app.get('/cadastro', function(req, res) {
+app.get('/cadastro', (req, res) => {
     res.sendFile(path.join(__dirname + "/../public/cadastro.html"));
 });
+
+app.post('/cadastro', (req, res) => {
+   res.redirect("/login");
+})
 
 app.listen(8081);
